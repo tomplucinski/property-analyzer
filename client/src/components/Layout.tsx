eaebd6ae34f06fb6d0137774dc11880b2982c56a
Tom Plucinski <tapluci@gmail.com>
2023-01-11 08:55:04 -0700

    Update picture for landing page

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,12 +1,10 @@
 import { Navbar } from './Navbar'
 import { Outlet } from 'react-router-dom'
-import { BottomNav } from './BottomNav'
 
 export function Layout() {
   return (
     <>
       <Navbar />
-      <BottomNav />
       <Outlet />
     </>
   )
