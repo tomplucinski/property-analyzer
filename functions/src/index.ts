324676c44532b7393db87c3c416a01884f1acce9
Tom Plucinski <tapluci@gmail.com>
2023-02-19 12:09:29 -0700

    Deploy test func

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
 admin.initializeApp();
 
@@ -11,7 +11,7 @@ exports.addMessage = functions.https.onRequest(async (req, res) => {
   // Push the new message into Firestore using the Firebase Admin SDK.
   const writeResult = await admin
     .firestore()
-    .collection("messages")
+    .collection('messages')
     .add({ original: original });
   // Send back a message that we've successfully written the message
   res.json({ result: `Message with ID: ${writeResult.id} added.` });
@@ -20,13 +20,13 @@ exports.addMessage = functions.https.onRequest(async (req, res) => {
 // Listens for new messages added to /messages/:documentId/original and creates an
 // uppercase version of the message to /messages/:documentId/uppercase
 exports.makeUppercase = functions.firestore
-  .document("/messages/{documentId}")
+  .document('/messages/{documentId}')
   .onCreate((snap, context) => {
     // Grab the current value of what was written to Firestore.
     const original = snap.data().original;
 
     // Access the parameter `{documentId}` with `context.params`
-    functions.logger.log("Uppercasing", context.params.documentId, original);
+    functions.logger.log('Uppercasing', context.params.documentId, original);
 
     const uppercase = original.toUpperCase();
 
