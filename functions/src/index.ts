d53fe071497070ad6dc89e5fcdbad1cda0bbbd6f
Tom Plucinski <tapluci@gmail.com>
2023-02-26 22:14:29 -0700

    Initialize analysis function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,17 +3,22 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp();
 
-// Take the text parameter passed to this HTTP endpoint and insert it into
-// Firestore under the path /messages/:documentId/original
 exports.addMessage = functions.https.onRequest(async (req, res) => {
-  // Grab the text parameter.
   const original = req.query.text;
-  // Push the new message into Firestore using the Firebase Admin SDK.
   const writeResult = await admin
     .firestore()
     .collection('messages')
     .add({ original: original });
-  // Send back a message that we've successfully written the message
+  res.json({ result: `Message with ID: ${writeResult.id} added.` });
+});
+
+exports.addAnalysis = functions.https.onRequest(async (req, res) => {
+  console.log('DATA', req.body);
+  const original = req.query.text;
+  const writeResult = await admin
+    .firestore()
+    .collection('properties')
+    .add({ original: original });
   res.json({ result: `Message with ID: ${writeResult.id} added.` });
 });
 
